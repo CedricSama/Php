8e7056a6efbc1d2717223e728535dc3e686085ae
Cedric <sajlksdfkg@msdlmglmfg/fr>
2017-10-31 11:11:02 +0100

    Ajout des assets bootstrap

--- Framework/SymfonyProjet/lddt/src/Lddt/MainBundle/Controller/DefaultController.php
+++ Framework/SymfonyProjet/lddt/src/Lddt/MainBundle/Controller/DefaultController.php
@@ -1,9 +1,30 @@
 <?php
     namespace Lddt\MainBundle\Controller;
+    use Lddt\MainBundle\Entity\Draw;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\Validator\Constraints\DateTime;
     class DefaultController extends Controller{
         public function indexAction(){
             return $this->render('LddtMainBundle:Default:index.html.twig');
         }
-        public function createAction(){}
+        public function createAction(){
+            //Instancier des dessins sans formulaire
+            //hydrater de valeurs
+            $draw = new Draw();
+            $draw->setTitle("Cochonou");
+            $draw->setDrawPath('cochonou.jpd');
+            $draw->setAuthorName('Maire');
+            $draw->setAuthorAvatarPath('marie-ico.jpg');
+            //Ces donnée on ete placé dans le constructeurs
+            // $draw->setIsOnline(true);
+            // $draw->setCreatedAt(new \DateTime());
+            // $draw->setUpdatedAt(new \DateTime());
+            $em = $this->get('doctrine')->getManager();
+            //verifie la requete
+            $em->persist($draw);
+            //apres tout les persist il execute la ou les requete(s)
+            $em->flush();
+            
+            return $this->redirect('lddt_main_homepage');
+        }
     }
---
+++ Framework/SymfonyProjet/lddt/src/Lddt/MainBundle/Entity/Draw.php
@@ -0,0 +1,214 @@
+<?php
+    namespace Lddt\MainBundle\Entity;
+    use Doctrine\ORM\Mapping as ORM;
+    /**
+     * Draw
+     *
+     * @ORM\Table(name="draw")
+     * @ORM\Entity(repositoryClass="Lddt\MainBundle\Repository\DrawRepository")
+     */
+    class Draw{
+        /**
+         * @var int
+         *
+         * @ORM\Column(name="id", type="integer")
+         * @ORM\Id
+         * @ORM\GeneratedValue(strategy="AUTO")
+         */
+        private $id;
+        /**
+         * @var string
+         *
+         * @ORM\Column(name="title", type="string", length=255)
+         */
+        private $title;
+        /**
+         * @var string
+         *
+         * @ORM\Column(name="draw_path", type="string", length=255)
+         */
+        private $drawPath;
+        /**
+         * @var bool
+         *
+         * @ORM\Column(name="is_online", type="boolean")
+         */
+        private $isOnline;
+        /**
+         * @var string
+         *
+         * @ORM\Column(name="author_name", type="string", length=255, nullable=true)
+         */
+        private $authorName;
+        /**
+         * @var string
+         *
+         * @ORM\Column(name="author_avatar_path", type="string", length=255, nullable=true)
+         */
+        private $authorAvatarPath;
+        /**
+         * @var \DateTime
+         *
+         * @ORM\Column(name="created_at", type="datetime")
+         */
+        private $createdAt;
+        /**
+         * @var \DateTime
+         *
+         * @ORM\Column(name="updated_at", type="datetime")
+         */
+        private $updatedAt;
+        
+        /**
+         * Draw constructor.
+         *
+         * @param bool $is_online
+         */
+        public function __construct($is_online = true){
+            $this->setCreatedAt(new \DateTime());
+            $this->setUpdatedAt(new \DateTime());
+            $this->setIsOnline($is_online);
+        }
+        /**
+         * Get id
+         *
+         * @return int
+         */
+        public function getId(){
+            return $this->id;
+        }
+        /**
+         * Set title
+         *
+         * @param string $title
+         *
+         * @return Draw
+         */
+        public function setTitle($title){
+            $this->title = $title;
+            return $this;
+        }
+        /**
+         * Get title
+         *
+         * @return string
+         */
+        public function getTitle(){
+            return $this->title;
+        }
+        /**
+         * Set drawPath
+         *
+         * @param string $drawPath
+         *
+         * @return Draw
+         */
+        public function setDrawPath($drawPath){
+            $this->drawPath = $drawPath;
+            return $this;
+        }
+        /**
+         * Get drawPath
+         *
+         * @return string
+         */
+        public function getDrawPath(){
+            return $this->drawPath;
+        }
+        /**
+         * Set isOnline
+         *
+         * @param boolean $isOnline
+         *
+         * @return Draw
+         */
+        public function setIsOnline($isOnline){
+            $this->isOnline = $isOnline;
+            return $this;
+        }
+        /**
+         * Get isOnline
+         *
+         * @return bool
+         */
+        public function getIsOnline(){
+            return $this->isOnline;
+        }
+        /**
+         * Set authorName
+         *
+         * @param string $authorName
+         *
+         * @return Draw
+         */
+        public function setAuthorName($authorName){
+            $this->authorName = $authorName;
+            return $this;
+        }
+        /**
+         * Get authorName
+         *
+         * @return string
+         */
+        public function getAuthorName(){
+            return $this->authorName;
+        }
+        /**
+         * Set authorAvatarPath
+         *
+         * @param string $authorAvatarPath
+         *
+         * @return Draw
+         */
+        public function setAuthorAvatarPath($authorAvatarPath){
+            $this->authorAvatarPath = $authorAvatarPath;
+            return $this;
+        }
+        /**
+         * Get authorAvatarPath
+         *
+         * @return string
+         */
+        public function getAuthorAvatarPath(){
+            return $this->authorAvatarPath;
+        }
+        /**
+         * Set createdAt
+         *
+         * @param \DateTime $createdAt
+         *
+         * @return Draw
+         */
+        public function setCreatedAt($createdAt){
+            $this->createdAt = $createdAt;
+            return $this;
+        }
+        /**
+         * Get createdAt
+         *
+         * @return \DateTime
+         */
+        public function getCreatedAt(){
+            return $this->createdAt;
+        }
+        /**
+         * Set updatedAt
+         *
+         * @param \DateTime $updatedAt
+         *
+         * @return Draw
+         */
+        public function setUpdatedAt($updatedAt){
+            $this->updatedAt = $updatedAt;
+            return $this;
+        }
+        /**
+         * Get updatedAt
+         *
+         * @return \DateTime
+         */
+        public function getUpdatedAt(){
+            return $this->updatedAt;
+        }
+    }
+
